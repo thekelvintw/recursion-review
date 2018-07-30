e69a46776f6a50677d1ff9a2dd14b0cea55da38c
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-30 11:58:28 -0700

    Strinify function for num, null, blooeam, string, Array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,35 @@
 
 // but you don't so you're going to write it from scratch:
 
+// Strategy:
+// declare the output varible emtpy string
+// check is the type of num, blooean, null, array, object, strings,  
+// function && undefined
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+// var obj = [2,3];
+  var output = '';
+  if (typeof obj === 'number') {
+    output += obj;
+  } else if (typeof obj === 'string') {
+    output += `"${obj}"`;
+  }else if (typeof obj === 'boolean') {
+    output += obj;
+  } else if (obj === null) {
+    output = 'null';
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]'
+    }
+    output += '[';
+    obj.forEach(function(element){
+      output += stringifyJSON(element);
+      output += ','
+    });
+    output = output.substring(0,output.length-1);
+    output += ']'
+  }
+  console.log('line 28', output)
+  return output;
 };
