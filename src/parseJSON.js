ab18ca8202653c54f685e0dd57886b0630c57c38
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-30 16:28:08 -0700

    Some strategy of parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,22 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
+// Strategy:
+// declare current index curr
+// declare next = curr+1
+// Loop through the strings
+// check first character (CH1) of input 
+  // if CH1 === [
+  // declare output = []
+        // if CH2 === ], return []
+        // if typeof next character = number 
+       //ouput.push(next character) 
+      // 
+
+
+
+
 var parseJSON = function(json) {
   // your code goes here
 };
