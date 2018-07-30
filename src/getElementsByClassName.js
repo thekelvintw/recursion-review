27216924dcac1be14e9d6ac743c915938e129206
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-30 15:54:36 -0700

    Solve the problem: get elements by className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,32 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//Strategy
+//  declare output array = [];
+//  search div if it has a className
+//  if the className === targetClassName
+//     output.push(div);
+//  search if the div has childNodes.length > 0
+//     forEach
+//        recursion
+//  return output array;
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className, parent, output) {
+  var output = output || [];
+  parent = parent || document.body;
+  if (parent === document.body && parent.className === className) {
+    output.push(parent);
+  }
+  var childrens = parent.children;
+  for(let i = 0; i < childrens.length; i++){
+    if (childrens[i].classList.contains(className)) {
+      output.push(childrens[i]);
+    }
+    if (childrens[i].children.length > 0) {
+      getElementsByClassName(className, childrens[i], output);
+    }
+  }
+  return output;
 };
